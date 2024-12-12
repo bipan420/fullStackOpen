95080616499e8136573806aee3bf3068ed10c428
bipan420 <40450884+bipan420@users.noreply.github.com>
2024-12-12 21:46:25 +0000

    Practicing fetch

--- practice2/notes/src/App.jsx
+++ practice2/notes/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import axios from 'axios'
+import { useEffect } from "react"
 
 const App = () => {
   const [notes, setNotes] = useState([{id: 1,
@@ -7,11 +9,21 @@ const App = () => {
   }])
   const [newNote, setNewNote] = useState('')
 
+  useEffect(() => {
+    console.log('effect')
+    axios.get('https://animated-space-capybara-pg46qwxvq5p3777q-3001.app.github.dev/notes')
+      .then(response => {
+        console.log("promise fulfilled")
+        setNotes(response.data)
+        
+      })
+  },[])
+  console.log('rendered', notes.length, 'notes')
+
   const noteFieldChange = (event) => {
     setNewNote(event.target.value)
   }
 
-  console.log("notes", notes)
 
   const createNote = (event) => {
     event.preventDefault()
@@ -30,6 +42,9 @@ const App = () => {
         <button type="submit">Save</button> 
       </form>
       {notes.map(note => <p key={note.id}>{note.content}</p>)}
+    {
+      
+    }
     </div>
   )
 }
